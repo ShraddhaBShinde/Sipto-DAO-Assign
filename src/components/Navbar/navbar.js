74c6aa4f5b3e5d257b07d4959b8b10ddfe6170de
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-10 18:44:23 +0530

    Sipto DAO Assign Setup

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <>
+            <div className = "navbar">
+                <a href="/home">Home</a>
+                <a href="/about">About</a>
+                <a href="/services">Service</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </>
+    );
+};
+
+export default Navbar;
