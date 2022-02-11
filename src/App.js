b0782623692b70c67aec6ef68264cfb054c6aecb
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-11 19:22:20 +0530

    Main Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+
+// Components
 import Navbar from "./components/Navbar/navbar";
+import MainPage from "./components/MainPage/mainpage";
 
 function App() {
   return (
     <>
         <div className="App">
             <Navbar />
+            <MainPage />
         </div>
     </>
   );
---
+++ src/components/MainPage/mainpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../../Styles/MainPage.css"
+
+function MainPage() {
+    return (
+        <>
+            <div className="mainPage">
+                <h1>Welcome to the Website</h1>
+            </div>
+        </>
+    );
+};
+
+export default MainPage;
