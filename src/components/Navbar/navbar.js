8ebbed0ec0cc7068cc2cbb41a58fe46d1185e743
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-12 20:33:39 +0530

    Navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -4,11 +4,11 @@ import "../../Styles/Navbar.css"
 function Navbar() {
     return (
         <>
-        <div className="navbar">
+        <div className="navbar shadow-lg p-3 mb-5 rounded bg-body">
             <div className="logo">
-                <img src="logo.png"></img>
+                <img src=""></img>
             </div>
-            <div className = "links ">
+            <div className ="links text-warning">
                 <a href="/home">Home</a>
                 <a href="/about">About</a>
                 <a href="/services">Service</a>
